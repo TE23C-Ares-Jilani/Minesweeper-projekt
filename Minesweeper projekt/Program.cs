f1b486e8d7f2930d8caaf83c3870917242272f95
Ares Jilani <ares.jilani@elev.ga.ntig>
2025-01-31 14:40:33 +0100

    first commit

--- Minesweeper projekt/Program.cs
+++ Minesweeper projekt/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Raylib_cs;
+
+class Minesweeper
+{
+    static void Main()
+    {
+        // start window for game with dimentions and title
+        Raylib.InitWindow(640, 480, "Minesweeper");
+        Raylib.SetTargetFPS(60);
+
+        while (!Raylib.WindowShouldClose()) // Main Game loop
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.RayWhite);
+            Raylib.DrawText("Minesweeper Game", 10, 10, 20, Color.Black);
+            Raylib.EndDrawing();
+        }
+        Raylib.CloseWindow(); // Close the window when the game loop exits
+    }
+}
